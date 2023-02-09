189492698ad7de6379e5be3de448370da2523f4a
fetsorn <fetsorn@gmail.com>
2023-02-10 02:25:31 +0400

    add docstrings

--- src/polyfill.js
+++ src/polyfill.js
@@ -9,7 +9,14 @@ function splitLines(str) {
   return str.split('\n').filter((line) => line !== '');
 }
 
-// return all lines in contentFile that match regex lines in patternFile
+/**
+ * This find lines in contentFile that match regex lines in patternFile.
+ * @name grepPolyfill
+ * @function
+ * @param {string} contents - The file contents.
+ * @param {string} regex - The regular expression in ripgrep format.
+ * @returns {string} - Search results.
+ */
 export function grepPolyfill(contentFile, patternFile) {
   const contentLines = splitLines(contentFile);
 
@@ -26,6 +33,12 @@ export function grepPolyfill(contentFile, patternFile) {
   return matches;
 }
 
+/**
+ * This generates a UUID.
+ * @name randomUUIDPolyfill
+ * @function
+ * @returns {string} - UUID compliant with RFC 4122.
+ */
 export function randomUUIDPolyfill() {
   return (
     [1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(
