744ba6da8d370b42656923d82e4211b02b1a78b5
Anton Davydov <fetsorn@gmail.com>
2025-11-09 13:15:04 +0300

    custom temp_dir

--- src/dataset/insert/mod.rs
+++ src/dataset/insert/mod.rs
@@ -19,14 +19,28 @@ struct Tablet {
 }
 
 async fn sort_file(filepath: &Path) -> Result<()> {
-    let temp_path = TempDir::new()?;
+    // must assign a variable to create the directory
+    // must assign inside the stream scope to keep the directory
+    let temp_d = TempDir::new();
+    
+    // on android <13 std::env::temp_dir() returns /data/local/tmp
+    // which is inaccessible on some android systems
+    let temp_d = match temp_d {
+       Err(e) if e.kind() == std::io::ErrorKind::PermissionDenied => {
+          TempDir::from_path(filepath.to_path_buf())
+       }
+       Err(e) => Err(e),
+       Ok(td) => Ok(td)
+    };
+
+    let temp_d = temp_d?;
 
     let filename = match filepath.file_name() {
         None => return Err(Error::from_message("unexpected missing filename")),
         Some(s) => s,
     };
 
-    let output = temp_path.as_ref().join(filename);
+    let output = temp_d.as_ref().join(filename);
 
     Sort::new(vec![filepath.to_path_buf()], output.to_path_buf()).sort();
 
