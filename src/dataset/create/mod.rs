971a5d362a7c87864031e1b9a38a205ede1d5fb3
Anton Davydov <fetsorn@gmail.com>
2026-01-16 10:45:43 +0300

    rename nested to bare

--- src/dataset/create/mod.rs
+++ src/dataset/create/mod.rs
@@ -1,8 +1,21 @@
 use crate::{Dataset, Result, Error};
 use std::path::PathBuf;
 
-pub async fn create(dir: &PathBuf, nested: bool) -> Result<Dataset> {
-    if (nested) {
+pub async fn create(dir: &PathBuf, bare: bool) -> Result<Dataset> {
+    if (bare) {
+        let version_path = dir.join(".csvs.csv");
+
+        let version_exists = std::fs::metadata(&version_path).is_ok();
+
+        if version_exists {
+            return Err(Error::from_message(""))
+        } else {
+            // write .csvs.csv
+            std::fs::write(&version_path, "csvs,0.0.2")?;
+
+            return Ok(Dataset { dir: dir.clone() });
+        }
+    } else {
         let nested_dir = dir.join("csvs");
 
         let nested_exists = std::fs::metadata(&nested_dir).is_ok();
@@ -20,19 +33,6 @@ pub async fn create(dir: &PathBuf, nested: bool) -> Result<Dataset> {
             std::fs::write(&version_path, "csvs,0.0.2")?;
 
             return Ok(Dataset { dir: nested_dir.clone() });
-        }
-    } else {
-        let version_path = dir.join(".csvs.csv");
-
-        let version_exists = std::fs::metadata(&version_path).is_ok();
-
-        if version_exists {
-            return Err(Error::from_message(""))
-        } else {
-            // write .csvs.csv
-            std::fs::write(&version_path, "csvs,0.0.2")?;
-
-            return Ok(Dataset { dir: dir.clone() });
         }
     }
 
